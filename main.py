548b2013461692ecd40d79a8dcfcf05e9d1b03c5
aloecrow <108513581+aloecrow@users.noreply.github.com>
2024-11-29 16:05:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import sys
+import csv
+import select_subject
+#import extract
+
+try:   
+    file = open('20231231.csv', 'r', encoding='CP949')
+
+except:
+    print("에러가 발생했습니다. 다시 시도해 주세요.")
+
+else:
+    selected_subject = select_subject.select(file)
+
+    print("선택된 과목 : ", selected_subject)
+    print("위 과목의 표준점수 분포 그래프는 다음과 같습니다.")
