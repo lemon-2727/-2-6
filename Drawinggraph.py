66300d26a557ff7e24c22a74a168b5e18ba7d7fc
bitmool05 <bitmool05@naver.com>
2024-11-30 23:49:38 +0900

    updated Drawinggraph.py with comments

--- Drawinggraph.py
+++ Drawinggraph.py
@@ -1,18 +1,21 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
 
+# 가공된 과목 안에서의 남자 배열, 여자 배열 순으로 매개인수 설정
+# score_graph(남자 배열, 여자 배열)
 def score_graph(arr_man, arr_woman):
+    #제목 한글로 바꾸기
     plt.rcParams['font.family'] = 'Malgun Gothic'
     plt.rcParams['axes.unicode_minus'] = False
 
-    stand_score_man = []
-    pp_num_man = []
-    stand_score_woman = []
-    pp_num_woman = []
+    # 남자, 여자 별로 표준점수와 인원수 리스트 생성
+    stand_score_man = [] # 남자 표준 점수
+    pp_num_man = [] # 남자 인원수
+    stand_score_woman = [] # 여자 표준 점수
+    pp_num_woman = [] # 여자 인원수
 
     # arr_man이랑 arr_woman 배열 갯수가 같음
+    # 배열 안에 있는 표준점수와 인원수 성별별로 분리해서 리스트에 append
     for i in range(len(arr_man)):
         stand_score_man.append(arr_man[i][2])
         pp_num_man.append(arr_man[i][3])
@@ -20,17 +23,21 @@ def score_graph(arr_man, arr_woman):
         stand_score_woman.append(arr_woman[i][2])
         pp_num_woman.append(arr_woman[i][3])
 
+    # 분석 용이하게 하기 위해 numpy로 바꿈
     x1 = np.array(stand_score_man)
     y1 = np.array(pp_num_man)
     x2 = np.array(stand_score_woman)
     y2 = np.array(pp_num_woman)
 
+    # 그래프 그리기
     plt.plot(x1, y1, label = 'man')
     plt.plot(x2, y2, label = 'woman')
 
+    # 그래프 설정
     plt.legend(loc='best', ncol=2)
     plt.xlabel("표준점수")
     plt.ylabel("인원 수")
     plt.title(f"2024학년도 수능 {arr_man[0][1]} 과목 분포")
 
+    # 그래프 표시
     plt.show()
