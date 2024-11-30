0d7e2d5d3ef3cce67b09f14170c9f8f11dd9f38e
aloecrow <108513581+aloecrow@users.noreply.github.com>
2024-11-30 18:30:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 import csv
-import select_subject
-#import extract
+import select_subject as ss
+import extract
+import Drawinggraph as dg
 
 try:   
     file = open('20231231.csv', 'r', encoding='CP949')
@@ -10,7 +11,16 @@ except:
     print("에러가 발생했습니다. 다시 시도해 주세요.")
 
 else:
-    selected_subject = select_subject.select(file)
+    selected_subjects = ss.select(file)
 
-    print("선택된 과목 : ", selected_subject)
+    print("선택된 과목 : %s - %s" %(selected_subjects[0], selected_subjects[1]))
     print("위 과목의 표준점수 분포 그래프는 다음과 같습니다.")
+
+    score_array = extract.get_arr('20231231.csv')
+
+    wanted_sub_f = extract.get_subj_gen(score_array, selected_subjects[0], selected_subjects[1], 0)
+    wanted_sub_m = extract.get_subj_gen(score_array, selected_subjects[0], selected_subjects[1], 0)
+
+    dg.score_graph(wanted_sub_f, wanted_sub_m)
+
+    
