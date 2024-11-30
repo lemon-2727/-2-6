eb113d397a4f1e9361438b89a2787f760fd49f4c
bitmool05 <bitmool05@naver.com>
2024-11-30 22:31:21 +0900

    updated Drawinggraph.py

--- Drawinggraph.py
+++ Drawinggraph.py
@@ -3,25 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
-def score_graph(arr, input_subj):
+def score_graph(arr_man, arr_woman):
     plt.rcParams['font.family'] = 'Malgun Gothic'
     plt.rcParams['axes.unicode_minus'] = False
+
     stand_score_man = []
     pp_num_man = []
     stand_score_woman = []
     pp_num_woman = []
-    for i in range(len(arr)):
-        if arr[i][1] == input_subj:
-            if arr[i][4] == 1:
-                stand_score_man.append(arr[i][2])
-            else:
-                stand_score_woman.append(arr[i][2])
-    for i in range(len(arr)):
-        if arr[i][1] == input_subj:
-            if arr[i][4] == 1:
-                pp_num_man.append(arr[i][3])
-            else:
-                pp_num_woman.append(arr[i][3])
+
+    # arr_man이랑 arr_woman 배열 갯수가 같음
+    for i in range(len(arr_man)):
+        if arr_man[i][1] == input_subj:
+            stand_score_man.append(arr_man[i][2])
+            pp_num_man.append(arr_man[i][3])
+
+            stand_score_woman.append(arr_woman[i][2])
+            pp_num_woman.append(arr_woman[i][3])
 
     x1 = np.array(stand_score_man)
     y1 = np.array(pp_num_man)
@@ -30,6 +28,10 @@ def score_graph(arr, input_subj):
 
     plt.plot(x1, y1, label = 'man')
     plt.plot(x2, y2, label = 'woman')
+
     plt.legend(loc='best', ncol=2)
-    plt.title(f"2024학년도 수능 {input_subj} 과목 분포")
+    plt.xlabel("표준점수")
+    plt.ylabel("인원 수")
+    plt.title(f"2024학년도 수능 {arr_man[0][1]} 과목 분포")
+
     plt.show()
